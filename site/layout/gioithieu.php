d3aefd2148f831f79dd1a84b716084e4e76b08c2
duc63607 <duc63607@gmail.com>
2023-07-25 10:57:11 +0700

    ok

---
+++ site/layout/gioithieu.php
@@ -0,0 +1,133 @@
+<div class=">
+    <hr>
+    <div class="">
+        <img class=" max-w-sreen-2xl" src="../upload/gthieu3.jpg" alt="">
+    <div class="text-center">
+        <h3 class="text-center  py-6  text-4xl " style="font-family: 'Courier New', Courier, monospace">SHOWROOM</h3>
+        <p>ROYAL CENTER hiện đang có mặt tại 6 showroom trên toàn quốc. Chọn ngay để biết showroom gần bạn nhất!</p>
+        <hr class="my-10">
+    </div>
+
+    <div class="mx-20">
+        <button id="btad" class="flex items-center my-10 bg-[#F8FAFC] shadow-cyan-500 shadow-lg px-8 py-3 rounded-lg ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-geo-alt-fill mx-4" viewBox="0 0 16 16">
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+            </svg>GIỚI THIỆU VỀ ROYAL CENTER</button>
+        <div class="bg-[#F4F6FA] rouded px-10 pb-10">
+            <p class="py-4  "><span class="text-3xl">ROYAL CENTER</span> là một hệ thống cửa hàng kinh doanh thời trang nữ. Cho phép người dùng tìm kiếm các sản phẩm đa dạng như túi, quần áo, trang sức,.... Để việc kinh doanh được mở rộng trên toàn quốc chúng tôi đã sáng lập ra website với thương hiệu Royal Center để người dùng có thể dễ dàng mua sắm online.
+                Điều đó sẽ giúp cho khách hàng của chúng tôi tiết kiệm thời gian với chỉ cần 1 cú click chuột.
+
+
+            </p>
+            <div class="about-repu">
+                <h2 class="text-2xl text-center my-10">
+                    TẠI SAO KHÁCH HÀNG LUÔN CHỌN ROYAL CENTER ?
+                </h2>
+                <div class="about-repu-list">
+                    <div class="about-repu-item">
+                        <i class="fa-solid fa-check about-repu-icon"></i>
+                        <span>Là chuỗi cửa hàng Giày Bóng Đá Chính Hãng uy tín hàng đầu
+                            tại Việt Nam, chuyên về giày bóng đá chính hãng và các phụ
+                            kiện bóng đá liên quan. Hiện chúng tôi có 4 CHI NHÁNH với đa
+                            dạng mẫu mã và nằm ở những vị trí trung tâm.
+                        </span>
+                    </div>
+                    <div class="about-repu-item">
+                        <i class="fa-solid fa-check about-repu-icon"></i>
+                        <span>
+                            Cam kết các sản phẩm chính hãng 100% được nhập khẩu Fullbox
+                            từ các nhà phân phối chính hãng tại Việt Nam – Quý khách có
+                            thể kiểm tra tem, nhãn trên sản phẩm, và tham khảo ý kiến
+                            mọi người trước khi quyết định mua hàng tại X-Shop.COM
+                        </span>
+                    </div>
+                    <div class="about-repu-item">
+                        <i class="fa-solid fa-check about-repu-icon"></i>
+                        <span>KHÁCH HÀNG ĐƯỢC BỒI THƯỜNG GẤP 10 LẦN NẾU SẢN PHẨM KHÔNG
+                            PHẢI CHÍNH HÃNG. Các sản phẩm đều được bảo hành tuyệt đối
+                            trong suốt thời gian sử dụng theo chính sách của từng nhà
+                            phân phối.
+                        </span>
+                    </div>
+                    <div class="about-repu-item">
+                        <i class="fa-solid fa-check about-repu-icon"></i>
+                        <span>Chúng tôi luôn tự tin sẽ mang lại sự hài lòng tuyệt đối cho
+                            khách hàng từ CHẤT LƯỢNG SẢN PHẨM đến DỊCH VỤ CHĂM SÓC KHÁCH
+                            HÀNG.
+                        </span>
+                    </div>
+                    <div class="about-repu-item">
+                        <i class="fa-solid fa-check about-repu-icon"></i>
+                        <span>
+                            X-Shop.COM luôn được nhiều khách hàng là tuyển thủ Quốc gia,
+                            cầu thủ chuyên nghiệp, ca sỹ, người nổi tiếng...tin tưởng
+                            lựa chọn .
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="about-motto">
+                <h2 class="text-2xl text-center my-10 ">
+                    PHƯƠNG CHÂM LÀM VIỆC CỦA ĐỘI NGŨ ROYAL CENTER
+                </h2>
+                <div class="about-motto-list">
+                    <div class="about-motto-item">
+                        <p>
+                            👉 Luôn cập nhật những mẫu mã giày bóng đá chính hãng mới
+                            nhất và những chính sách khuyến mãi – ưu đãi giảm giá hấp
+                            dẫn.
+                        </p>
+                    </div>
+                    <div class="about-motto-item">
+                        <p>
+                            👉 Chính sách bảo hành tuyệt đối với sản phẩm và đổi hàng
+                            trong vòng 7 ngày, thanh toán tiện lợi thông qua dịch vụ COD
+                            ( nhận hàng – thanh toán ) XEM CHI TIẾT TẠI ĐÂY
+                        </p>
+                    </div>
+                    <div class="about-motto-item">
+                        <p>
+                            👉 Đội ngũ CSKH tư vấn nhiệt tình, kỹ càng sẽ giúp khách
+                            hàng có thể lựa chọn cho mình một đôi giày phù hợp và hài
+                            lòng nhất.
+                        </p>
+                    </div>
+                    <div class="about-motto-item">
+                        <p>
+                            👉 Tập thể X-Shop.COM hiểu rằng sự hài lòng của khách hàng
+                            chính là mục tiêu to lớn nhất, là thước đo thành công của
+                            một thương hiệu.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="my-10">
+        <div class=" mx-20">
+            <div class="text-center space-y-4">
+                <h1 class="text-center   text-3xl " style="font-family: 'Courier New', Courier, monospace">TRẢI NGHIỆM MUA SẮM TẠI SHOWROOM</h1>
+                <p>Mua sắm trực tiếp tại hệ thống showroom để có những trải nghiệm dịch vụ tốt nhất.</p>
+            </div>
+            <div class="grid grid-cols-3 gap-10 my-10">
+                <div class="">
+                    <a href=""><img class="shadow-2xl shadow-cyan-500/50" src="../upload/gthieu4.webp" alt=""></a>
+                    <h3 class="text-center my-10">KHÔNG GIAN THỜI TRANG VÀ HIỆN ĐẠI</h3>
+                </div>
+                <div class="">
+                    <a href=""><img class="shadow-2xl shadow-cyan-500/50" src="../upload/gthieu5.webp" alt=""></a>
+                    <h3 class="text-center my-10">MUA SẮM THOẢI MÁI VÀ TIỆN LỢI</h3>
+                </div>
+                <div class="">
+                    <a href=""><img class="shadow-2xl shadow-cyan-500/50" src="../upload/gthieu6.webp" alt=""></a>
+                    <h3 class="text-center my-10">ĐÓN TIẾP ÂN CẦN, TƯ VẤN CHUYÊN NGHIỆP</h3>
+                </div>
+                <div class=""></div>
+            </div>
+
+
+        </div>
+        <hr>
+    </div>
+    <hr>
+</div>
